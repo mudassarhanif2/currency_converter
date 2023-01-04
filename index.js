e99d54a27e98bd8bf43d8b9e8efb52ba27568054
Mudassar Hanif <mudassarhanif2@gmail.com>
2023-01-04 22:20:13 +0500

    working fine

---
+++ index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let conversion = {
+    "PKR": {
+        'USD': 0.0044,
+        'GBP': 0.0037,
+        'PKR': 1
+    },
+    "GBP": {
+        'USD': 1.21,
+        'PKR': 271.79,
+        'GBP': 1,
+    },
+    "USD": {
+        'PKR': 225.50,
+        'GBP': 0.83,
+        'USD': 1
+    }
+};
+const answer = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'from',
+        choices: ['PKR', 'USD', 'GBP'],
+        message: 'Select your courrency: '
+    },
+    {
+        type: 'list',
+        name: 'to',
+        choices: ['PKR', 'USD', 'GBP'],
+        message: 'Select your convertion courrency: '
+    },
+    {
+        type: 'number',
+        name: 'amount',
+        choices: ['PKR', 'USD', 'GBP'],
+        message: 'Enter amount you want to convert  : '
+    }
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    let result = conversion[from][to] * amount;
+    console.log(`Your conversion from ${amount} ${from} to ${to} is ${result}${to}`);
+}
+else {
+    console.log('Please enter valid values');
+}
+// answer
